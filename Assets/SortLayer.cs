682c217195167a9afd98f8d92b552e8b23fc69dc
adrienponcet <adr.poncet@gmail.com>
2017-03-01 19:09:53 +0100

    Pouple et début d'integration

---
+++ Assets/SortLayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SortLayer : MonoBehaviour {
+
+    [SerializeField]
+    string layerName;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        renderer.sortingLayerName = layerName;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
